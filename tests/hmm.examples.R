680779b45348667411dc6fe7a60b309d1a757c8b
mt269 <mt269@3e584e52-61a1-4911-b546-322c73959ec4>
2010-09-23 18:53:20 +0000

    some cosmetic changes to make lines in manual not run off edge
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/rphast/trunk@3397 3e584e52-61a1-4911-b546-322c73959ec4

--- tests/hmm.examples.R
+++ tests/hmm.examples.R
@@ -22,9 +22,11 @@ conservedMod$tree <- rescale.tree(neutralMod$tree, 0.3)
 #'
 # create a simple phylo-HMM
 state.names <- c("neutral", "conserved")
-h <- hmm(matrix(c(0.99, 0.01, 0.01, 0.99), nrow=2, dimnames=list(state.names, state.names)),
+h <- hmm(matrix(c(0.99, 0.01, 0.01, 0.99), nrow=2,
+                dimnames=list(state.names, state.names)),
                 eq.freq=c(neutral=0.9, conserved=0.1))
-scores <- score.hmm(align, mod=list(neutral=neutralMod, conserved=conservedMod),
+scores <- score.hmm(align, mod=list(neutral=neutralMod,
+                             conserved=conservedMod),
                     hmm=h, states="conserved")
 # try an alternate approach of comparing likelihoods of genes 
 feats <- read.feat("gencode.ENr334.gff")
--- tests/plot.examples.R
+++ tests/plot.examples.R
@@ -9,7 +9,8 @@ featTrack <- feat.track(f, "basic feature track")
 f <- addIntrons.feat(f)
 geneTrack <- gene.track(f, "gene track")
 plot.track(list(featTrack, geneTrack))
-plot.track(list(featTrack, geneTrack, geneTrack, geneTrack, geneTrack), xlim=c(14800, 16000))
+plot.track(list(featTrack, geneTrack, geneTrack, geneTrack, geneTrack),
+           xlim=c(14800, 16000))
 
 #' plot.gene
 exampleArchive <- system.file("extdata", "examples.zip", package="rphast")
