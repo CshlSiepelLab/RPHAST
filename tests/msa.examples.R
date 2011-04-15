8ac0402c8e8efb82c771462cf0c18a5a4c1a3ae3
mt269 <mt269@3e584e52-61a1-4911-b546-322c73959ec4>
2011-04-15 19:55:01 +0000

    fixed small bug in previous commit
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/rphast/trunk@3914 3e584e52-61a1-4911-b546-322c73959ec4

--- tests/msa.examples.R
+++ tests/msa.examples.R
@@ -472,7 +472,7 @@ plot.msa(m[1:3,1:40])
 plot.msa(m[,1:100])
 plot.msa(m[,1:50], refseq=NULL)
 plot.msa(m[,1:50], refseq=NULL, nuc.text=rep(paste(rep("ASDFG", 10), sep="", collapse=""), 2),
-         nuc.text.col=c("black", "red"), nuc.text.pos=c("top")
+         nuc.text.col=c("black", "red"), nuc.text.pos=c("top"))
 rm(m)
 unlink("ENr334.maf")
 
