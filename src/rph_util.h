666ef62721aef74168338f8a4383629b1f0c298b
Rebecca Hassett <hassett@cshl.edu>
2024-11-25 17:25:25 -0500

    update util to prevent macro collision

--- src/rph_util.h
+++ src/rph_util.h
@@ -1,6 +1,6 @@
 //NOTE: this file is only included by files in src/lib/rphast
+#define COMPLEX R_COMPLEX_OVERRIDE
 #include <Rdefines.h>
-#undef COMPLEX
 #undef Matrix
 #undef nrows
 #include <misc.h>
